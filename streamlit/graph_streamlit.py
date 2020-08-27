a9285d7b5a88ed2bf5fd5e681cd5993de699555c
Bruno Dorneles <bruno.silva.dorneles@gmail.com>
2020-08-27 18:37:09 -0400

    Add Similarity Threshold

--- streamlit/graph_streamlit.py
+++ streamlit/graph_streamlit.py
@@ -7,20 +7,25 @@ import streamlit.components.v1 as stc
 def st_graph(html):
     graph_file = codecs.open(html, 'r')
     page = graph_file.read()
-    stc.html(page, width=700, height=700)
+    stc.html(page, width=900, height=1000)
 
 graph_file = srsly.read_json('graph.json')
 
 st.title('Medical Graph')
+st.sidebar.subheader('Similarity Threshold')
+threshold = st.sidebar.slider("",0.0, 1.0)
 
-graph = Network(height="750px", width="100%", bgcolor="#E5ECF6", font_color="#444444", directed=True, notebook=True)
+graph = Network(width='100%',bgcolor="#F6F8FB", font_color="#444444", directed=True, heading='', notebook=True)
 for link in graph_file['links']:
     similarity = float(link['weight'])
-    if similarity >= 0.2 and similarity != 1.0:
+    if similarity >= threshold and similarity != 1.0:
         graph.add_node(link['source'], size=14, color = '#2169AD')
         graph.add_node(link['target'], size=10, color = '#78CABC')
         graph.add_edge(link['source'], link['target'], weight = round(similarity * 100))
 
-graph.show_buttons(filter_=['physics'])
+#graph.show_buttons(filter_=['physics'])
+graph.force_atlas_2based()
 graph.show('graph.html')
 st_graph('graph.html')
+
+st.sidebar
